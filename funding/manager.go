69dbb57a01e7fc5b21015ca781a4cbdf478bd2dd
Nishant Bansal <nishant.bansal.282003@gmail.com>
2025-04-03 23:06:14 +0530

    cleanup-10
    Signed-off-by: Nishant Bansal <nishant.bansal.282003@gmail.com>

--- funding/manager.go
+++ funding/manager.go
@@ -1302,6 +1302,12 @@ func (f *Manager) handleConfirmation(channel *channeldb.OpenChannel,
 		&channel.FundingOutpoint.Hash, chanFundingScript, 1,
 		channel.BroadcastHeight(),
 	)
+	if err != nil {
+		log.Errorf("Unable to register for confirmation of "+
+			"ChannelPoint(%v): %v", channel.FundingOutpoint,
+			err)
+		return
+	}
 
 	var confDetails *chainntnfs.TxConfirmation
 
@@ -1343,14 +1349,14 @@ func (f *Manager) handleConfirmation(channel *channeldb.OpenChannel,
 	// useful to resume the opening process in case of restarts. We set the
 	// opening state before we mark the channel opened in the database,
 	// such that we can receover from one of the db writes failing.
-	// err = f.saveChannelOpeningState(
-	// 	&fundingPoint, markedConfirm, &shortChanID,
-	// )
-	// if err != nil {
-	// 	log.Errorf("error setting channel state to "+
-	// 		"markedConfirm: %v", err)
-	// 	return
-	// }
+	err = f.saveChannelOpeningState(
+		&fundingPoint, markedConfirm, &shortChanID,
+	)
+	if err != nil {
+		log.Errorf("error setting channel state to "+
+			"markedConfirm: %v", err)
+		return
+	}
 
 	err = channel.MarkConfirmedScid(shortChanID)
 	if err != nil {
